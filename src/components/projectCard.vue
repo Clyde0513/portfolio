f68620d815d5d3636efc7233e9089faccb05f418
Clyde0513 <iclyde.villacrusis@gmail.com>
2024-12-24 16:41:37 -0800

    Fixing project section

--- src/components/projectCard.vue
+++ src/components/projectCard.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="main-container">
-        <div class="project-card" v-for="(items) in projectData.ProjectsArray" v-bind:key="items">
+        <div class="project-card" v-for="(items, index) in projectData.ProjectsArray" v-bind:key="items">
             <h2>{{ items.Title }}</h2>
             <img :src="items.Image" alt="">
             <img :src="items.Image1" alt="">
             <p class='about-me'>{{ items.About }}</p>
             <p class='about-me'>{{ items.About1 }}</p>
-            <img :src="items.Image2" alt="">
-            <img :src="items.Image3" alt="">
-            <img :src="items.Image4" alt="">
+            <img v-if="index === 0" :src="items.Image2" alt="">
+            <img v-if="index === 0" :src="items.Image3" alt="">
+            <img v-if="index === 0" :src="items.Image4" alt="">
             <div class="links">
                 <a :href="items.Link" target="_blank"><img src="../assets/github.png" alt="Github Logo"></a>
             </div>
